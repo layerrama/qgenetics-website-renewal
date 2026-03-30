2a3b1e5fc32207e1b5184d4469a037db8ace5ab7
layerrama <cowell@qgenetics.co.kr>
2026-03-30 11:43:00 +0900

    Update next.config.mjs
    하위페이지 리다이렉션

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/page/:path*',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },reactStrictMode: true,
   images: {
     remotePatterns: [
       {
