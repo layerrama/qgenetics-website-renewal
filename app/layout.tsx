210778002506f561d824599962d0940342869a2b
layerrama <cowell@qgenetics.co.kr>
2026-03-31 09:40:24 +0900

    Update layout.tsx google analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { LanguageProvider } from "@/context/LanguageContext";
 import "./globals.css";
 import Providers from "./providers";
 
+import Script from 'next/script';
 export const metadata: Metadata = {
   title: "Qgenetics",
   description: "Qgenetics corporate site"
@@ -13,6 +14,15 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
   return (
     <html lang="en">
       <body>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-8WKG8CZ0NM" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8WKG8CZ0NM');
+          `}
+        </Script>
         <Providers>
           <LanguageProvider>{children}</LanguageProvider>
         </Providers>
@@ -20,3 +30,4 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
     </html>
   );
 }
+
